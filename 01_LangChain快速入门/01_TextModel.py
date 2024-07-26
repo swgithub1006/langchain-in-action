28611937fb2faa47624f01e3cbf90ef249d1589f
sw1006 <15928017242@163.com>
2024-07-26 14:04:34 +0800

    格式化代码

--- 01_LangChain快速入门/01_TextModel.py
+++ 01_LangChain快速入门/01_TextModel.py
@@ -3,6 +3,7 @@ https://time.geekbang.org/column/intro/100617601
 作者 黄佳'''
 
 from dotenv import load_dotenv  # 用于加载环境变量
+
 load_dotenv()  # 加载 .env 文件中的环境变量
 
 # import os
@@ -20,12 +21,12 @@ load_dotenv()  # 加载 .env 文件中的环境变量
 # print(response.choices[0].text.strip())
 
 from openai import OpenAI
+
 client = OpenAI()
 
-response = client.completions.create(
-  model="gpt-3.5-turbo-instruct",
-  temperature=0.5,
-  max_tokens=100,
-  prompt="请给我的花店起个名")
+response = client.completions.create(model="gpt-3.5-turbo-instruct",
+                                     temperature=0.5,
+                                     max_tokens=100,
+                                     prompt="请给我的花店起个名")
 
 print(response.choices[0].text.strip())

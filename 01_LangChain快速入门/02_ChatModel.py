c718f29a081aae427619ae915c3f5b1934fbd2e6
sw1006 <15928017242@163.com>
2024-07-26 14:01:12 +0800

    格式化代码

--- 01_LangChain快速入门/02_ChatModel.py
+++ 01_LangChain快速入门/02_ChatModel.py
@@ -3,6 +3,7 @@ https://time.geekbang.org/column/intro/100617601
 作者 黄佳'''
 
 from dotenv import load_dotenv  # 用于加载环境变量
+
 load_dotenv()  # 加载 .env 文件中的环境变量
 
 # import os
@@ -24,18 +25,24 @@ load_dotenv()  # 加载 .env 文件中的环境变量
 
 # print(response.choices)
 
-
 from openai import OpenAI
+
 client = OpenAI()
 
-response = client.chat.completions.create(  
-  model="gpt-4",
-  messages=[
-        {"role": "system", "content": "You are a creative AI."},
-        {"role": "user", "content": "请给我的花店起个名"},
-    ],
-  temperature=0.8,
-  max_tokens=60
-)
+response = client.chat.completions.create(model="gpt-4",
+                                          messages=[
+                                              {
+                                                  "role":
+                                                  "system",
+                                                  "content":
+                                                  "You are a creative AI."
+                                              },
+                                              {
+                                                  "role": "user",
+                                                  "content": "请给我的花店起个名"
+                                              },
+                                          ],
+                                          temperature=0.8,
+                                          max_tokens=60)
 
 print(response.choices[0].message.content)
